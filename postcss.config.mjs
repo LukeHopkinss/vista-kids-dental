3269dd7d20821d9306278ec1d915792c43e74cc4
Luke Hopkins <luke.hopkins@outlook.com>
2025-09-21 17:53:38 -0400

    edited tailwind config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -7,9 +7,9 @@ import aspectRatio from '@tailwindcss/aspect-ratio';
 export default {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',     // harmless if you don't use /pages
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',     
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,mdx}',       // keep if you use a /src structure
+    './src/**/*.{js,ts,jsx,tsx,mdx}',      
   ],
   theme: {
     extend: {
