1bec52c9d2d23c7c552da2985315ed7351719386
Luke Hopkins <luke.hopkins@outlook.com>
2025-09-21 17:59:15 -0400

    final tailwind config fix

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,22 +1,5 @@
-// tailwind.config.mjs
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
-import aspectRatio from '@tailwindcss/aspect-ratio';
-
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',     
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,mdx}',      
-  ],
-  theme: {
-    extend: {
-      colors: {
-        brand: '#0284c7', // optional brand color
-      },
-    },
+  plugins: {
+    "@tailwindcss/postcss": {},
   },
-  plugins: [forms, typography, aspectRatio],
 };
